177bd6b82d9e0e68bff77a672d78d83eb5f97662
Bulrock <maksim.volozhinskij@gmail.com>
2022-06-30 23:22:39 +0300

    feat: implement reverse-int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let result = '';
+    let nAbs = Math.abs(n);
+    let nAbsString = nAbs.toString();
+  for (let i = -1; i >= -nAbsString.length; i--){
+    let slicedNumeral = nAbsString[nAbsString.length+i]
+    result = `${result}${slicedNumeral}`
+  }
+  return result;
 }
